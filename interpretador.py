7f5883fa7f623a8a4ab1a06dea4a70638f490eb5
Enio-Antonio <enioaclizieri@gmail.com>
2025-01-30 00:15:08 -0300

    Adiciona leitura do arquivo do código a partir da linha de comando

--- interpretador.py
+++ interpretador.py
@@ -1,11 +1,25 @@
+from sys import argv
+
+nome_arquivo = argv[1]
+
+# Tratando o arquivo
+try:
+    nome_arquivo.split(".")[1] != "ec"
+except:
+    raise Exception("Verifique se o arquivo.\n")
+
+if nome_arquivo.split(".")[1] != "ec":
+    raise Exception("A extensão do arquivo não é .ec\n")
+
 codigos_e = ["release", ">", "capture", "portal", "receive", "final", "calc"] # Gramática super complicada
 codigos_p = ["print", "end_print", "input", "def", "var", "end", "operacao"]
 
-memoria = [] # Eu vou simplesmente deixar o Python manejar a memória
+memoria = {} # Eu vou simplesmente deixar o Python manejar a memória
 
 programa = []
 
-codigo = "release < information 1 information 2 > capture thisvar calc < 1 + 2 + 3 > final"
+codigo = open(nome_arquivo, 'r').read()
+#codigo = "release < information 1 information 2 > calc < 1 + 2 + 3 > final"
 codigo_tokenizado = codigo.split()
 
 for i in range(len(codigo_tokenizado)):
@@ -23,10 +37,7 @@ contador_print_debug = 0
 
 while True:
     word = programa[contador]
-    if contador_print_debug < 3:
-        print(word)
-    
-    contador_print_debug += 1
+    #print(f"Essa é a keyword: {word}")
 
     if word == "print":
         contador += 2
@@ -39,18 +50,24 @@ while True:
         arg_list.pop()
         print_string = " ".join(arg_list)
         print(print_string)
+        contador -= 1
     elif word == "input":
         contador += 1
         var_para_input = programa[contador]
         input_value = input()
-        memoria.append({input_value: var_para_input})
+        memoria[var_para_input] = input_value
         contador += 1
-        print(memoria)
     elif word == "operacao":
         contador += 2
         arg_op = programa[contador]
-        print(f"Esse é o arg_op: {arg_op}")
+        #print(f"Esse é o arg_op: {arg_op}")
+        temp = []
         while arg_op != "end_print":
-            pass 
+            contador += 1
+            arg_op = programa[contador]
+            if (arg_op != 'end_print'):
+                temp.append(arg_op)
+        #print(f"Lista de numeros para somar:")
     elif word == "end":
         break
+    contador += 1
