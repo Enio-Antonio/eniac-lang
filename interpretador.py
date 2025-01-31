52c5a52527570ceba2dd17647c1e72d7e5703b79
Enio-Antonio <enioaclizieri@gmail.com>
2025-01-30 21:48:00 -0300

    Adiciona suporte a variáveis

--- interpretador.py
+++ interpretador.py
@@ -11,15 +11,15 @@ except:
 if nome_arquivo.split(".")[1] != "ec":
     raise Exception("A extensão do arquivo não é .ec\n")
 
-codigos_e = ["release", ">", "capture", "portal", "receive", "final", "calc"] # Gramática super complicada
-codigos_p = ["print", "end_print", "input", "def", "var", "end", "operacao"]
+# A keyword 'sum' não tem muito sentido se não for usada com um variável, dado que o resultado não será salva em nenhum lugar sem isso
+codigos_e = ["release", ">", "capture", "portal", "receive", "final", "sum", "repeat_n_times"] # Gramática super complicada
+codigos_p = ["print", "end_print", "input", "def", "var", "end", "somar", "loop"]
 
 memoria = {} # Eu vou simplesmente deixar o Python manejar a memória
 
 programa = []
 
 codigo = open(nome_arquivo, 'r').read()
-#codigo = "release < information 1 information 2 > calc < 1 + 2 + 3 > final"
 codigo_tokenizado = codigo.split()
 
 for i in range(len(codigo_tokenizado)):
@@ -57,17 +57,26 @@ while True:
         input_value = input()
         memoria[var_para_input] = input_value
         contador += 1
-    elif word == "operacao":
+    elif word == "somar":
         contador += 2
         arg_op = programa[contador]
         #print(f"Esse é o arg_op: {arg_op}")
         temp = []
-        while arg_op != "end_print":
-            contador += 1
+        while arg_op != "end_print": 
             arg_op = programa[contador]
-            if (arg_op != 'end_print'):
+            if (arg_op != 'end_print' and arg_op != "+"):
                 temp.append(arg_op)
-        #print(f"Lista de numeros para somar:")
+            contador += 1
+        print(f"Lista de numeros para somar: {temp}")
+        contador -= 1
+    elif word == "var":
+        contador += 1
+        var_nome = programa[contador]
+        contador += 2
+        memoria[var_nome] = programa[contador]
+        contador += 1
+        print(f"Esse é o nome da variável: {var_nome}")
+        print(f"Esse é o valor da variável: {memoria[var_nome]}")
     elif word == "end":
         break
     contador += 1
